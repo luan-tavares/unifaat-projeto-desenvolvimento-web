099b72d8a1130d5f2e6bb73d61a91246d594539a
Luan Lourenço <luan.tavares.lourenco@gmail.com>
2025-05-05 15:25:53 -0300

    script.js

--- aula-09/public/script.js
+++ aula-09/public/script.js
@@ -1,5 +1,23 @@
-const dropZone = document.getElementById("dropZone");
-const fileInput = document.getElementById("fileInput");
-const fileList = document.getElementById("fileList");
+(function () {
 
+    const dropZone = document.getElementById("dropZone");
+    const fileInput = document.getElementById("fileInput");
+    const fileList = document.getElementById("fileList");
 
+    /** Listeners */
+
+    /*** --------- */
+
+    /** helpers */
+
+    function showStatus(message, type = "success") { }
+
+    /** async helpers */
+
+    async function handleUpload(file) { }
+
+    async function fetchFileList() { }
+
+    fetchFileList();
+
+})();
